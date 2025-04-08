ac0428067431fdec42e2b3977e489a7754a37c34
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-04-08 19:03:25 +0200

    Cleaned up ContactMe and its finished

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -2,16 +2,15 @@ const ContactMe = () => {
   return (
     <section className="contact--section" id="Contact">
       <div>
-        <p className="sub--title">Get in touch</p>
-        <h2>Contact Me</h2>
+        <h2>Kontakta mig</h2>
         <p className="text-lg">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, ea.
+          Kontakta gärna mig här eller direkt via LinkedIn.
         </p>
       </div>
       <form className="contact--form--container">
         <div className="container">
           <label htmlFor="first-name" className="contact--label">
-            <span className="text-md">First Name</span>
+            <span className="text-md">Förnamn</span>
             <input
               type="text"
               className="contact--input text-md"
@@ -21,7 +20,7 @@ const ContactMe = () => {
             />
           </label>
           <label htmlFor="last-name" className="contact--label">
-            <span className="text-md">Last Name</span>
+            <span className="text-md">Efternamn</span>
             <input
               type="text"
               className="contact--input text-md"
@@ -31,7 +30,7 @@ const ContactMe = () => {
             />
           </label>
           <label htmlFor="email" className="contact--label">
-            <span className="text-md">Email</span>
+            <span className="text-md">E-post</span>
             <input
               type="email"
               className="contact--input text-md"
@@ -41,7 +40,7 @@ const ContactMe = () => {
             />
           </label>
           <label htmlFor="phone-number" className="contact--label">
-            <span className="text-md">Phone Number</span>
+            <span className="text-md">Telefon nummer</span>
             <input
               type="number"
               className="contact--input text-md"
@@ -51,30 +50,30 @@ const ContactMe = () => {
             />
           </label>
         </div>
-        <label htmlFor="choose-topic" className="contact--label">
+        {/* <label htmlFor="choose-topic" className="contact--label"> addera denna vid senare tillfälle
           <span className="text-md">Choose a topic</span>
           <select id="choose-topic" className="contact--input text-md">
             <option>Select One...</option>
-            <option>Item 1</option>
-            <option>Item 2</option>
-            <option>Item 3</option>
+            <option>Item Praktikplats</option>
+            <option>Item Bygg mig en hemsida</option>
+            <option>Item Något mera?</option>
           </select>
-        </label>
+        </label> */}
         <label htmlFor="message" className="contact--label">
-          <span className="text-md">Message</span>
+          <span className="text-md">Meddelande</span>
           <textarea
             className="contact--input text-md"
             id="message"
             rows="8"
-            placeholder="Type your message..."
+            placeholder="Skriv ditt meddelande..."
           />
         </label>
-        <label htmlFor="checkbox" className="checkbox--label">
+        {/* <label htmlFor="checkbox" className="checkbox--label"> Lägg till denna vid senare tillfälle iaf den behövs
           <input type="checkbox" required name="checkbox" id="checkbox" />
           <span className="text-sm">I accept the terms</span>
-        </label>
+        </label> */}
         <div>
-          <button className="btn btn-primary contact--form--btn">Submit</button>
+          <button className="btn btn-primary contact--form--btn">Skicka</button>
         </div>
       </form>
     </section>
