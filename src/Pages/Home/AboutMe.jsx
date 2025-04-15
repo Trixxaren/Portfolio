4c85cc3651216b8f3505c5735c11e312221cc8a3
Robin Vikström <robin.m.e.vikstrom@gmail.com>
2025-04-15 15:28:51 +0200

    Changed colors, techflow colors used

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
           <h1 className="skills--section--heading">Om mig</h1>
-          <p className="hero--section-description">
+          <p className="text-md">
             Jag är en lösningsorienterad och nyfiken person med ett stort
             intresse för teknik, människor och problemlösning. Just nu utbildar
             jag mig till front-end utvecklare på Jensen Yrkeshögskola, där jag
@@ -18,14 +18,14 @@ const AboutMe = () => {
             färdigheter med mina tidigare erfarenhet inom marknadsföring,
             kundrelationer och affärsutveckling.
           </p>
-          <p className="hero--section-description">
+          <p className="text-md">
             Innan jag valde att byta bana jobbade jag som Key Account Manager på
             Viaplay Group Radio, där jag ansvarade för strategiska samarbeten
             med några av Sveriges största företag. Jag har en stark känsla för
             att bygga relationer, skapa värde och driva projekt från idé till
             resultat, något jag nu tar med mig in i techvärlden.
           </p>
-          <p className="hero--section-description">
+          <p className="text-md">
             Jag är en målinriktad doer som trivs i team, men också tar stort
             eget ansvar. Jag drivs av utveckling, både på det personliga och
             tekniska planet och just nu söker jag en praktikplats där jag får
